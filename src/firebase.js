4ea436b56f718a25445c21018b9708500cf3c1c9
dailycodemode <steedmanman@gmail.com>
2021-11-13 19:30:50 +0000

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGESENDERID,
+  appId: process.env.REACT_APP_APPID,
+  measurementId: process.env.REACT_APP_MEASUREMENTID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+
+export function signup(email, password) {
+    return createUserWithEmailAndPassword(auth, email, password);
+}
+
+
+// Custom Hook
+export function useAuth() {
+    const [ currentUser, setCurrentUser ] = useState();
+  
+    useEffect(() => {
+      const unsub = onAuthStateChanged(auth, user => setCurrentUser(user));
+      return unsub;
+    }, [])
+  
+    return currentUser;
+  }
+
+export function logout() {
+    return signOut(auth);
+}
+
+export function login(email, password) {
+return signInWithEmailAndPassword(auth, email, password);
+}
